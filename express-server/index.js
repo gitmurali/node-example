b0e33c23583a07e86e077474ba058088e4fa5a12
gitmurali <murali.nutalapatii@gmail.com>
2022-05-08 11:58:24 +0100

    feature: add cluster mode

--- express-server/index.js
+++ express-server/index.js
@@ -1,10 +1,27 @@
-const express = require("express");
-const app = express();
+const cluster = require("cluster");
 
-app.use("/", (req, res) => {
-  res.send("hi there");
-});
+if (cluster.isMaster) {
+  //execute index.js again in child mode.
+  cluster.fork();
+  cluster.fork();
+  cluster.fork();
+  cluster.fork();
+} else {
+  //child act like a server and do nothing else
+  const express = require("express");
+  const app = express();
 
-app.listen(3000, () => {
-  console.log("listening on 3000");
-});
+  function doWork(duration) {
+    const start = Date.now();
+    while (Date.now() - start < duration) {}
+  }
+
+  app.use("/", (req, res) => {
+    doWork(5000);
+    res.send("hi there");
+  });
+
+  app.listen(3000, () => {
+    console.log("listening on 3000");
+  });
+}
